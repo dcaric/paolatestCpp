e69042b1242732bb0f3a29002d140560d1b2fd62
dcaric <dcaric@me.com>
2021-09-30 23:40:41 +0200

    Moved code into the function

--- zbrojNiza.cpp
+++ zbrojNiza.cpp
@@ -10,34 +10,43 @@
 
 using namespace std;
 
+
+
+string leptirFunkcija(string limun) {
+    try
+    {
+        printf("\n Upiši broj:");
+        cin >> limun;
+        //int strToInt = stoi(limun);
+        //printf("strToNum= : %s \n", limun.c_str());
+
+        int suma  = 0;
+        for (int i= 1; i<= stoi(limun); i ++) suma = suma +i;
+        
+        printf("Zbroj brojeva je: %i \n", suma);
+    }
+    catch(const std::exception& e)
+    {
+        //std::cerr << e.what() << '\n';
+        if (limun != "stop") printf("Upisao si krivi podatak. Rijec se ne moze pretvoriti u broj ili ta rijes nije STOP !\n");
+    }
+    return limun;
+}
+
+
+
 int main(int argc, const char * argv[]) {
     
 
     string limun = "";
     
     while (limun != "stop") {
-
-        try
-        {
-            printf("\n Upiši broj:");
-            cin >> limun;
-            int strToInt = stoi(limun);
-            printf("strToNum= : %s \n", limun.c_str());
-
-            int suma  = 0;
-            for (int i= 1; i<= strToInt; i ++) {
-                suma = suma +i;
-            }
-            printf("Zbroj brojeva je: %i \n", suma);
-        }
-        catch(const std::exception& e)
-        {
-            //std::cerr << e.what() << '\n';
-            if (limun != "stop") printf("Upisao si krivi broj, to nije broj niti rijec STOP !\n");
-        }
-    
+        limun = leptirFunkcija(limun);
     }
 
-    printf("PROGRAM JE GOTOV");
+    printf("PROGRAM JE GOTOV \n");
+
+
 
 }
+
