bba9e7d227b2bdecd333fd8d9daeca984a3f07dc
dcaric <dcaric@me.com>
2021-10-03 14:41:27 +0200

    Before explanation var scope

--- zbrojNiza.cpp
+++ zbrojNiza.cpp
@@ -12,41 +12,30 @@ using namespace std;
 
 
 
-string leptirFunkcija(string limun) {
+void leptirFunkcija(string limun) {
     try
     {
         printf("\n Upiši broj:");
         cin >> limun;
-        //int strToInt = stoi(limun);
-        //printf("strToNum= : %s \n", limun.c_str());
-
         int suma  = 0;
         for (int i= 1; i<= stoi(limun); i ++) suma = suma +i;
-        
         printf("Zbroj brojeva je: %i \n", suma);
     }
     catch(const std::exception& e)
     {
-        //std::cerr << e.what() << '\n';
         if (limun != "stop") printf("Upisao si krivi podatak. Rijec se ne moze pretvoriti u broj ili ta rijes nije STOP !\n");
     }
-    return limun;
 }
 
 
 
 int main(int argc, const char * argv[]) {
     
-
     string limun = "";
     
     while (limun != "stop") {
-        limun = leptirFunkcija(limun);
+        leptirFunkcija(limun);
     }
-
     printf("PROGRAM JE GOTOV \n");
-
-
-
 }
 
